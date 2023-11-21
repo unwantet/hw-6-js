ba1bc3c7fafc0e859d5bd4bffc5366db22f087b4
unwantet <darkblezer59@gmail.com>
2023-11-21 18:54:08 +0500

    added 6-hw

---
+++ main.js
@@ -0,0 +1,52 @@
+
+
+// const random = Math.floor(Math.random() * 50) + 1;
+
+// let number = alert(+prompt('Guess a number from 1 to 50: '));
+
+// function guessNumber() {
+
+//     if(number == random) {
+//        alert('You guessed the correct number.');
+//     }
+
+//     else {
+//         for(let a = 1 ; a<=10;a++){
+//             if(number !== random) {
+//                 number = alert(prompt('Guess a number from 1 to 50: '));
+//             }
+//         }
+
+//         alert("you not guessed my number xaxax looser😂🥲")
+//     }
+
+
+//   }
+
+// guessNumber();
+
+//////////////////////////////////////////////////////////////
+
+// Math.E           // Eyler raqamini qaytaradi
+// Math.PI          // PIni qaytaradi
+// Math.SQRT2       // 2 ning kvadrat ildizini qaytaradi
+// Math.SQRT1_2    // 1/2 kvadrat ildizini qaytaradi
+// Math.LN2        // 2 ning natural logarifmini qaytaradi
+// Math.LN10       // 10 ning natural logarifmini qaytaradi
+// Math.LOG2E      // E ning 2 ta logarifmini qaytaradi
+// Math.LOG10E     // E ning 10 ta logarifmini qaytaradi
+
+
+
+/////////////////////////////////////////////////////////////
+
+// Math.pow(x, y)x qiymatini y kuchiga qaytaradi
+
+//Math.abs(x)x ning mutlaq (musbat) qiymatini qaytaradi
+
+
+// Math.sin(90 * Math.PI / 180); // 1ni qaytaradi (90 daraja sinus)
+
+//
+
+  
